43631443e2fdc84078056e58dd86398799834630
jinheeyi <jinhee.yi27@gmail.com>
2020-02-09 06:54:17 +1000

    Add global color and size (layout)

---
+++ constants/Color.js
@@ -0,0 +1,6 @@
+export const BG_COLOR = "black";
+export const WHITE = "white";
+export const HT_COLOR = "#FFDF2C";
+export const INACTIVE_COLOR = "#A9A9A9";
+export const ACTIVE_COLOR = WHITE;
+export const GREY_COLOR = "#bdc3c7";
